c2630eb9783c88f41d6df511768ab939f6467fb6
CodeMemilk <rsalon300@gmail.com>
2026-09-23 21:40:25 +0545

    Updated triggers.sql
    Added an additional trigger

--- WPMS/database/schema/06_create_triggers.sql
+++ WPMS/database/schema/06_create_triggers.sql
@@ -25,3 +25,30 @@ END //
 
 DELIMITER ;
 
+USE work_progress_management_system;
+
+DELIMITER //
+
+CREATE TRIGGER before_checklist_update
+BEFORE UPDATE ON Task_CheckList_Item
+FOR EACH ROW
+BEGIN
+
+    IF NEW.is_completed = TRUE
+       AND OLD.is_completed = FALSE THEN
+
+        SET NEW.completed_at = CURRENT_TIMESTAMP;
+
+    END IF;
+
+
+    IF NEW.is_completed = FALSE THEN
+
+        SET NEW.completed_at = NULL;
+
+    END IF;
+
+END //
+
+DELIMITER ;
+
